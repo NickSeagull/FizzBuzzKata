71698bbdf764b168cc2bd86bca7c30418bd27979
Nikita Tchayka <nikitatchayka@gmail.com>
2015-07-28 14:08:39 +0100

    Delete the test TODOs
    Forgot to do it while I was writing the tests

--- src/test/scala-2.11/com/nickseagull/FizzBuzzSpec.scala
+++ src/test/scala-2.11/com/nickseagull/FizzBuzzSpec.scala
@@ -4,14 +4,6 @@ import org.scalatest.FlatSpec
 import org.scalatest.matchers.MustMatchers
 import com.nickseagull.FizzBuzz._
 
-/*
-  TODO: Return a string made up from a sequence of numbers with the following conditions:
-    - If the number is divisible by 2, it will be changed by "Fizz"
-    - If the number is divisible by 3, it will be changed by "Buzz"
-    - If these 2 conditions are met, the number will be changed by "FizzBuzz"
-    - If not, the number will stay the same
- */
-
 class FizzBuzzSpec extends FlatSpec with MustMatchers {
 
   "The FizzBuzz function" must "leave the number 1 as it is" in {
