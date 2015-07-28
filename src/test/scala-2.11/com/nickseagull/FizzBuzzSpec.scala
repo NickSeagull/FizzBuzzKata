5242d48419533fe4cdbc799f0c2ba28aaa542aa1
Nikita Tchayka <nikitatchayka@gmail.com>
2015-07-28 12:59:04 +0100

    Add TODO

--- src/test/scala-2.11/com/nickseagull/FizzBuzzSpec.scala
+++ src/test/scala-2.11/com/nickseagull/FizzBuzzSpec.scala
@@ -1,9 +1,16 @@
 package com.nickseagull
 
 import org.scalatest._
-/**
- * Created by Nikita on 28/07/2015.
+
+/*
+  TODO: Return a string made up from a sequence of numbers with the following conditions:
+    - If the number is divisible by 2, it will be changed by "Fizz"
+    - If the number is divisible by 3, it will be changed by "Buzz"
+    - If these 2 conditions are met, the number will be changed by "FizzBuzz"
+    - If not, the number will stay the same
  */
+
 class FizzBuzzSpec extends FlatSpec with ShouldMatchers {
+
 
 }
